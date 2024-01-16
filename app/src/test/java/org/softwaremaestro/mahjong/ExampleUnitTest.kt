cabb186b8f69730a4938069c64223dc3c5aa7a0b
haechan29 <haechan.im@gmail.com>
2024-01-16 16:05:25 +0900

    test fun getLayout

--- app/src/test/java/org/softwaremaestro/mahjong/ExampleUnitTest.kt
+++ app/src/test/java/org/softwaremaestro/mahjong/ExampleUnitTest.kt
@@ -14,4 +14,29 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun putNumberAndGetLayout() {
+        assertEquals(1, getLayout(4).size)
+        assertEquals(2, getLayout(8).size)
+    }
+
+    @Test
+    fun layoutConsistsOfPairOfNumbers() {
+        val layout = getLayout(8)
+        for (i in 1 until 4) {
+            assertEquals(2, layout.flatten().count { it == i })
+        }
+    }
+
+    private fun getLayout(number: Int): Array<Array<Int>> {
+        val shuffledNumbers = mutableListOf<Int>().apply {
+            repeat(2) {
+                for (i in 1..number / 2) add(i)
+            }
+        }.shuffled()
+        return Array(number / 4) {
+            shuffledNumbers.slice(it * 4 until (it + 1) * 4).toTypedArray()
+        }
+    }
 }

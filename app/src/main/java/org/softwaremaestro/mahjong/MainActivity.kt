8d2f86689aa30762b442321451247d9ec75adbd5
haechan29 <haechan.im@gmail.com>
2024-01-16 15:40:10 +0900

    add fadeOut animation

--- app/src/main/java/org/softwaremaestro/mahjong/MainActivity.kt
+++ app/src/main/java/org/softwaremaestro/mahjong/MainActivity.kt
@@ -4,25 +4,37 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,26 +63,40 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MahjongCard(frontResId: Int, backResId: Int) {
     var rotated by remember { mutableStateOf(false) }
+    var isVisible by remember { mutableStateOf(true) }
     val mRotationY by animateFloatAsState(
         targetValue = if (rotated) 180f else 0f,
         animationSpec = tween(500)
     )
-    Card(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.White)
-            .padding(20.dp)
-            .graphicsLayer { rotationY = mRotationY }
-            .clickable { rotated = !rotated },
-        shape = RoundedCornerShape(10.dp)
-    ) {
-        Image(
-            modifier = Modifier.fillMaxSize(),
-            painter = painterResource(
-                id = if (mRotationY <= 90) frontResId else backResId
-            ),
-            contentDescription = null
+    AnimatedVisibility(
+        visible = isVisible,
+        exit = fadeOut(
+            animationSpec = tween(500)
         )
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White)
+                .padding(20.dp)
+                .graphicsLayer { rotationY = mRotationY }
+                .clickable {
+                    if (isCorrect()) {
+                        isVisible = !isVisible
+                    } else {
+                        rotated = !rotated
+                    }
+                },
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(
+                    id = if (mRotationY <= 90) frontResId else backResId
+                ),
+                contentDescription = null
+            )
+        }
     }
 }
 
@@ -83,4 +109,8 @@ fun MahjongCardw() {
             R.drawable.ic_launcher_background
         )
     }
+}
+
+fun isCorrect(): Boolean {
+    return listOf(true, false).random()
 }

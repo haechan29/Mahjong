be0c2edb040d75bd17c97304f8bc08d310e839db
haechan29 <haechan.im@gmail.com>
2024-01-16 15:16:29 +0900

    add rotateY animation

--- app/src/main/java/org/softwaremaestro/mahjong/MainActivity.kt
+++ app/src/main/java/org/softwaremaestro/mahjong/MainActivity.kt
@@ -1,11 +1,14 @@
 package org.softwaremaestro.mahjong
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,6 +16,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -31,7 +38,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MahjongCard(R.drawable.ic_launcher_background)
+                    MahjongCard(
+                        R.drawable.ic_launcher_foreground,
+                        R.drawable.ic_launcher_background
+                    )
                 }
             }
         }
@@ -39,18 +49,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MahjongCard(painterResourceId: Int) {
+fun MahjongCard(frontResId: Int, backResId: Int) {
+    var rotated by remember { mutableStateOf(false) }
+    val mRotationY by animateFloatAsState(
+        targetValue = if (rotated) 180f else 0f,
+        animationSpec = tween(500)
+    )
     Card(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.White)
             .padding(20.dp)
-        ,
+            .graphicsLayer { rotationY = mRotationY }
+            .clickable { rotated = !rotated },
         shape = RoundedCornerShape(10.dp)
     ) {
         Image(
             modifier = Modifier.fillMaxSize(),
-            painter = painterResource(id = painterResourceId),
+            painter = painterResource(
+                id = if (mRotationY <= 90) frontResId else backResId
+            ),
             contentDescription = null
         )
     }
@@ -60,6 +78,9 @@ fun MahjongCard(painterResourceId: Int) {
 @Composable
 fun MahjongCardw() {
     MahjongTheme {
-        MahjongCard(R.drawable.ic_launcher_background)
+        MahjongCard(
+            R.drawable.ic_launcher_foreground,
+            R.drawable.ic_launcher_background
+        )
     }
 }
